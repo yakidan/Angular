b1ada6122d93035f18c254d9cea766c0c068b213
Daniil Yak <8spider-man@mail.ru>
2020-08-07 17:07:19 +0300

    Learn ngFor

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -7,15 +7,20 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  toggle:any=false;
-
-
-
-
-
-
-
-
-
+  arr = [1, 1, 2, 3, 5, 8, 13]
+  objs = [
+    {
+      title: 'Post 1', author: 'Daniil', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Dan', text: 'lorem 2'},
+      ]
+    },
+    {
+      title: 'Post 2', author: 'Dina', comments: [
+        {name: 'Step', text: 'lorem 1'},
+        {name: 'Kira', text: 'lorem 2'},
+      ]
+    },
+  ]
 
 }
