c0cbb23d48713eb5aae4a96e29be29c1d523daa8
Daniil Yak <8spider-man@mail.ru>
2020-08-07 14:24:20 +0300

    Learn ngModule

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -7,28 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  title = 'text Initial'
 
-  constructor() {
-
-  }
-
-  inputValue = " ";
-
-  title = 'Dynamic title';
-  arr = [1, 2, 3];
-  obj = { a: 2 };
-  img = 'https://c7.hotpng.com/preview/933/638/374/react-components-javascript-redux-mobile-app-react-js.jpg';
-
-  onInput(event: KeyboardEvent) {
-    console.log('Event')
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
-
-  onClick() {
-    console.log('Click')
+  onInput(event: any) {
+    this.title = event.target.value
   }
 }
