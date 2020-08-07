2a887cad0cd12123dd38667943ec54fb12472400
Daniil Yak <8spider-man@mail.ru>
2020-08-07 17:14:14 +0300

    Learn pipes

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -7,20 +7,6 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 8, 13]
-  objs = [
-    {
-      title: 'Post 1', author: 'Daniil', comments: [
-        {name: 'Max', text: 'lorem 1'},
-        {name: 'Dan', text: 'lorem 2'},
-      ]
-    },
-    {
-      title: 'Post 2', author: 'Dina', comments: [
-        {name: 'Step', text: 'lorem 1'},
-        {name: 'Kira', text: 'lorem 2'},
-      ]
-    },
-  ]
+  now: Date = new Date()
 
 }
