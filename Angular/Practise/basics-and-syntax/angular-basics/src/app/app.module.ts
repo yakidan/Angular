4c619a8afb8969fe1c2e0565498bc94de93ebabd
Daniil Yak <8spider-man@mail.ru>
2020-08-08 11:44:50 +0300

    Directives: getting access to an element

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.module.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.module.ts
@@ -1,17 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
 import {StyleDirective} from "./directives/style.directive";
-
-import { Style2Directive } from './directives/style2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirective,
-    Style2Directive
 
   ],
   imports: [
@@ -21,4 +18,5 @@ import { Style2Directive } from './directives/style2.directive';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
