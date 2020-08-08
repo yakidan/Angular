d6c27206d4aba22e6b6f818b88783abc5f8c5beb
Daniil Yak <8spider-man@mail.ru>
2020-08-08 11:53:24 +0300

    Directives: Add Listeners

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/directives/style.directive.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/directives/style.directive.ts
@@ -1,10 +1,24 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive({selector: '[appStyle]'})
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
-    this.r.setStyle(el.nativeElement, 'color', 'blue')
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
     //el.nativeElement.style.color = 'red'
     console.log(el)
   }
+
+  @HostListener('click', ['$event.target']) onClick(Event) {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'red')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null)
+  }
+
+
 }
