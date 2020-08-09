cbb97998be274ff9dbf105edc86cc5e00f55ce09
Daniil Yak <8spider-man@mail.ru>
2020-08-09 11:13:25 +0300

    RxJS: Simple example rxjs( use interval())

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -1,12 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import {Component} from '@angular/core';
+import {interval} from 'rxjs'
 
-export interface Post {
-  title: string
-  text: string
-
-
-}
+// import {} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -16,14 +11,13 @@ export interface Post {
 
 export class AppComponent {
 
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved')
-    }, 4000)
-  })
-  date: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(() => {
-      obs.next(new Date())
-    }, 1000)
-  })
+  constructor() {
+
+    const intervalStream$ = interval(1000)
+
+    intervalStream$.subscribe((value)=>{
+      console.log(value)
+    })
+  }
+
 }
