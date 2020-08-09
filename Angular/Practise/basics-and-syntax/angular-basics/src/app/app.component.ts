0a308da701a2755b0f408506d9c9c7616c571b9a
Daniil Yak <8spider-man@mail.ru>
2020-08-09 16:53:41 +0300

    Form and Validation: Validators

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,15 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 
 export class AppComponent implements OnInit {
+
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null),
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required],),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(6)]),
     })
   }
 
@@ -20,8 +25,10 @@ export class AppComponent implements OnInit {
 
 
   submit() {
-    console.log('Form:', this.form)
-    const formData = {...this.form.value}
-    console.log("Form data:",formData)
+    if (this.form.valid) {
+      console.log('Form:', this.form)
+      const formData = {...this.form.value}
+      console.log("Form data:", formData)
+    }
   }
 }
