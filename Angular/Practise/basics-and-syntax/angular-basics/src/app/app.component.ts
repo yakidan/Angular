d80ff81331e2b2375de5186045d7abab153553ed
Daniil Yak <8spider-man@mail.ru>
2020-08-09 12:24:22 +0300

    Services: local services

--- Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {Subscription, Subject} from 'rxjs'
 import {AppCounterService} from "./services/app-counter.service";
+import {LocalCounterService} from "./services/local-counter.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
 
 export class AppComponent {
-  constructor(private appCounterService: AppCounterService) {
+  constructor(private appCounterService: AppCounterService,
+              private localCounterService:LocalCounterService) {
 
   }
 }
--- Angular/Practise/basics-and-syntax/angular-basics/src/app/services/app-counter.service.ts
+++ Angular/Practise/basics-and-syntax/angular-basics/src/app/services/app-counter.service.ts
@@ -13,5 +13,5 @@ export class AppCounterService {
   decrease() {
     this.counter--
   }
-  
+
 }
